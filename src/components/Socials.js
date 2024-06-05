611a5ebc4336a6037e59ac92c1e9ad08612313e5
Rose <rvleeuwen@DigitalIndividuals.com>
2024-06-05 19:32:27 +0200

    happy with socials component

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -3,24 +3,24 @@ export default function Socials() {
         <div className="bg-neutral-800 bg-opacity-40 w-80 h-52 rounded-[25px] py-4 px-5 ">
             <h1 className="font-bold">Socials</h1>
             <div className="flex items-center justify-center w-full">
-                <div className="grid grid-cols-3">
-                    <div className="w-full h-auto p-5 mb-1 flex items-center">
-                        <img src="/discord.png" className="w-96 h-auto"></img>
+                <div className="grid gap-9 grid-cols-3">
+                    <div className="w-full h-auto flex items-center justify-center">
+                        <img src="/discord.png" className="object-scale-down h-14 w-14"></img>
                     </div>
-                    <div className="w-full h-auto p-5 mb-1 flex items-center">
-                        <img src="/github_edit.png" className="w-96 h-auto"></img>
+                    <div className="w-full h-auto flex items-center justify-center">
+                        <img src="/github_edit.png" className="object-scale-down h-14 w-14"></img>
                     </div>
-                    <div className="w-full h-auto p-5 mb-1 flex items-center">
-                        <img src="/Steam_icon_logo.svg.png" className="w-96 h-auto"></img>
+                    <div className="w-full h-auto flex items-center justify-center">
+                        <img src="/Steam_icon_logo.svg.png" className="object-scale-down h-14 w-14"></img>
                     </div>
-                    <div className="w-full h-auto p-5 mb-1 flex items-center">
-                        <img src="/discord.png" className="w-96 h-auto"></img>
+                    <div className="w-full h-auto flex items-center justify-center">
+                        <img src="/car.jpg" className="object-scale-down h-14 w-14"></img>
                     </div>
-                    <div className="w-full h-auto p-5 mb-1 flex items-center">
-                        <img src="/github_edit.png" className="w-96 h-auto"></img>
+                    <div className="w-full h-auto flex items-center justify-center">
+                        <img src="/Rose.png" className="object-scale-down h-14 w-14"></img>
                     </div>
-                    <div className="w-full h-auto p-5 mb-1 flex items-center">
-                        <img src="/Steam_icon_logo.svg.png" className="w-96 h-auto"></img>
+                    <div className="w-full h-auto flex items-center justify-center">
+                        <img src="/Steam_icon_logo.svg.png" className="object-scale-down h-14 w-14"></img>
                     </div>
                 </div>
             </div>
