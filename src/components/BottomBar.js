1c50059195c453c99c690ef71e55a2e53a0134ce
Rose <rvleeuwen@DigitalIndividuals.com>
2024-06-05 19:46:26 +0200

    animates socials

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,13 +1,20 @@
+import React, { useState } from 'react';
 import Socials from "./Socials";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+
 export default function BottomBar() {
+    const [socialActive, setSocialActive] = useState(false);
     let date = new Date().toLocaleDateString();
-    let ToggleIsActive = false;
+
+    const handleEthernetClick = () => {
+        setSocialActive(!socialActive);
+    };
+
     return (
         <>
             <div className='bottom-0 h-14 bg-black flex items-center absolute inset-x-0 bg-opacity-50 justify-between'>
                 <div className='flex ml-4'>
-                    <div className='hover:bg-opacity-50 transition duration-150 ease-in-out hover:bg-neutral-700 rounded-full p-1.5 w-14 h-12 flex items-center'> {/** TODO: Fix roundness */}
+                    <div className='hover:bg-opacity-50 transition duration-150 ease-in-out hover:bg-neutral-700 rounded-full p-1.5 w-14 h-12 flex items-center'>
                         <img src="/Rose.png" className='p-1'></img>
                     </div>
                 </div>
@@ -16,7 +23,7 @@ export default function BottomBar() {
                         <p className='text-lg'>NLD</p>
                     </div>
                     <div className='hover:bg-opacity-50 transition duration-150 ease-in-out hover:bg-neutral-700 rounded-full w-12 h-12 flex items-center justify-center'>
-                        <img src="/etherneticon_edit.png" className='object-scale-down h-9' onClick={ToggleIsActive}></img>
+                        <img src="/etherneticon_edit.png" className='object-scale-down h-9' onClick={handleEthernetClick}></img>
                     </div>
 
                     <div className='hover:bg-opacity-50 transition duration-150 ease-in-out hover:bg-neutral-700 rounded-full w-12 h-12 flex items-center justify-center'>
@@ -27,13 +34,19 @@ export default function BottomBar() {
                     </div>
                 </div>
             </div>
-            <div className="bottom-20 right-16 absolute">
-                <motion.div initial={{ y: 50 }}
-                    animate={{ y: 0 }}
-                    transition={{ duration: 0.5, delay: 2 }}>
-                    <Socials />
-                </motion.div>
-            </div>
+            <AnimatePresence>
+                {socialActive && (
+                    <motion.div
+                        initial={{ y: 25, opacity: 0.25 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        exit={{ y: 25, opacity: 0 }}
+                        transition={{ duration: 0.25 }}
+                        className="bottom-20 right-16 absolute"
+                    >
+                        <Socials />
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </>
-    )
-} 
+    );
+}
