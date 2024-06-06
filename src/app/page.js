5ae1f0a8f41d33f04745305454f71a5a93dff3d1
Rose van Leeuwen <rvleeuwen@digitalindividuals.com>
2024-06-06 13:28:42 +0200

    hitting the GRIDdy

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@
 import BootScreenAnim from '@/components/BootScreen';
 import BottomBar from '@/components/BottomBar';
 import TopBar from '@/components/TopBar';
-import Socials from '@/components/Socials';
 import { motion } from 'framer-motion'
 import { useState, useEffect } from 'react';
 
@@ -22,21 +21,42 @@ export default function Home() {
           <BootScreenAnim />
         </div>
       ) : (
-        <div className="">
+        <div>
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.3 }}
             className="bg-[url('/Rosey-stars.png')] bg-cover bg-center h-screen">
+            <div className='flex justify-start p-10'>
+              <div className='grid grid-rows-5 grid-flow-col w-screen gap-y-52'>
+                <div className='flex justify-center items-center'> as1</div>
+                <div className='flex justify-center items-center'>1 asdf</div>
+                <div className='flex justify-center items-center'>1</div>
+                <div className='flex justify-center items-center'>1</div>
+                <div className='flex justify-center items-center'>1</div>
+
+                <div className='flex justify-center items-center'>1</div>
+                <div className='flex justify-center items-center'>1</div>
+                <div className='flex justify-center items-center'>1</div>
+                <div className='flex justify-center items-center'>1</div>
+                <div className='flex justify-center items-center'>1</div>
+                
+                <div className='flex justify-center items-center'>1</div>
+                <div className='flex justify-center items-center'>1</div>
+                <div className='flex justify-center items-center'>1</div>
+                <div className='flex justify-center items-center'>1</div>
+                <div className='flex justify-center items-center'>1</div>
+                
+                <div></div>
+              </div>
+            </div>
+            <div className='md:hidden block'>
+              <TopBar />
+            </div>
+            <div className='md:block hidden'>
+              <BottomBar />
+            </div>
           </motion.div>
-          
-          <div className='md:block hidden'>
-            <BottomBar />
-            
-          </div>
-          <div className='md:hidden block'>
-            <TopBar/>
-          </div>
         </div>
       )}
     </div>
