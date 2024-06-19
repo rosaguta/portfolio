bed0e2802d114778c97e6a2c4921ede17841ba79
Rose van Leeuwen <rvleeuwen@digitalindividuals.com>
2024-06-19 11:21:14 +0200

    firefox icon with functionallity

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,11 +1,11 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import Socials from "./Socials";
 import { motion, AnimatePresence } from "framer-motion";
 import Music from './Music';
 import StartMenu from './StartMenu';
 
 
-export default function BottomBar() {
+export default function BottomBar({ BrowserActive, handleBrowserVisibility }) {
     const [socialActive, setSocialActive] = useState(false);
     const [musicActive, setMusicActive] = useState(false)
     const [startMenuActive, setStartMenuActive] = useState(false)
@@ -25,11 +25,16 @@ export default function BottomBar() {
 
     return (
         <>
-            <div className='bottom-0 h-14 bg-black flex items-center absolute inset-x-0 bg-opacity-50 justify-between'>
-                <div className='flex ml-4'>
+            <div className='bottom-0 h-14 bg-black flex items-center  absolute inset-x-0 bg-opacity-50 justify-between'>
+                <div className='flex ml-4 gap-x-2'>
                     <div className='hover:bg-opacity-50 transition duration-150 ease-in-out hover:bg-neutral-700 rounded-full p-1.5 w-14 h-12 flex items-center' onClick={handleStartMenuCick}>
                         <img src="/Rose.png" className='p-1'></img>
                     </div>
+                    {BrowserActive &&(
+                        <div className='hover:bg-opacity-50 transition duration-150 ease-in-out hover:bg-neutral-700 rounded-full p-1.5 w-14 h-12 flex items-center' onClick={handleBrowserVisibility}>
+                            <img src="/firefox.png" className='p-1'></img>
+                        </div>
+                    )}
                 </div>
                 <div className='flex mr-4 '>
                     <div className='flex items-center w-12 h-12'>
