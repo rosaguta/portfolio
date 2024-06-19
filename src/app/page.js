51fd60ea619802cfabfc6d79d19a3e0186343d3f
Rose van Leeuwen <rvleeuwen@digitalindividuals.com>
2024-06-19 11:20:45 +0200

    browser animates in and out accordingly

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,16 @@ import BootScreenAnim from '@/components/BootScreen';
 import BottomBar from '@/components/BottomBar';
 import TopBar from '@/components/TopBar';
 import Browser from '@/components/TotallyRealBrowser';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useState, useEffect } from 'react';
+import useWindowDimensions from '@/functions/useWindowDimensions';
 
 export default function Home() {
   const [showBootScreen, setShowBootScreen] = useState(true);
-  const [isBrowserVisible, setIsBrowserVisible] = useState(true);
+  const [isBrowserActive, setIsBrowserActive] = useState(true);
   const [isBrowserMinimized, setIsBrowserMinimized] = useState(false);
   const [isBrowserMaximized, setIsBrowserMaximized] = useState(false);
+  const { width, height } = useWindowDimensions();
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -20,7 +22,7 @@ export default function Home() {
   }, []);
 
   const handleCloseClick = () => {
-    setIsBrowserVisible(false);
+    setIsBrowserActive(false);
   };
 
   const handleMinimizeClick = () => {
@@ -45,20 +47,30 @@ export default function Home() {
             transition={{ duration: 0.3 }}
             className="bg-[url('/Rosey-stars.png')] bg-cover bg-center h-screen"
           >
-            {isBrowserVisible && (
-              <Browser
-                handleCloseClick={handleCloseClick}
-                handleMinimizeClick={handleMinimizeClick}
-                handleSizeClick={handleSizeClick}
-                isMinimized={isBrowserMinimized}
-                isMaximized={isBrowserMaximized}
-              />
+            {isBrowserActive && (
+              <AnimatePresence>
+                {!isBrowserMinimized && (
+                  <motion.div
+                    initial={{y: height / 1.8, x: -width / 3, opacity: 0.25 }}
+                    animate={{x:0 ,y:0 , opacity: 1 }}
+                    exit={{ y: height/1.8, x:-width/3, opacity: 0 }}
+                    transition={{ duration: 0.15 }}>
+                    <Browser
+                      handleCloseClick={handleCloseClick}
+                      handleMinimizeClick={handleMinimizeClick}
+                      handleSizeClick={handleSizeClick}
+                      isMinimized={isBrowserMinimized}
+                      isMaximized={isBrowserMaximized}
+                    />
+                  </motion.div>
+                )}
+              </AnimatePresence>
             )}
             <div className="md:hidden block">
               <TopBar />
             </div>
             <div className="md:block hidden">
-              <BottomBar />
+              <BottomBar BrowserActive={isBrowserActive} handleBrowserVisibility={handleMinimizeClick} />
             </div>
           </motion.div>
         </div>
