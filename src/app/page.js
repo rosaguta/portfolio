e85dbd537c1a1f83cb151612c3f1e40a9bd12815
Rose <rvleeuwen@DigitalIndividuals.com>
2024-06-23 23:11:57 +0200

    works i guess? doesnt scale well

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ import useWindowDimensions from '@/functions/useWindowDimensions';
 
 export default function Home() {
   const [showBootScreen, setShowBootScreen] = useState(true);
-  const [isBrowserActive, setIsBrowserActive] = useState(true);
+  const [isBrowserActive, setIsBrowserActive] = useState(false);
   const [isBrowserMinimized, setIsBrowserMinimized] = useState(false);
   const [isBrowserMaximized, setIsBrowserMaximized] = useState(false);
   const { width, height } = useWindowDimensions();
@@ -51,9 +51,9 @@ export default function Home() {
               <AnimatePresence>
                 {!isBrowserMinimized && (
                   <motion.div
-                    initial={{y: height / 1.8, x: -width / 2, opacity: 0.25 , scale:0}}
-                    animate={{x:0 ,y:0 , opacity: 1,scale:1 }}
-                    exit={{ y: height/1.8, x:-width/2, opacity: 0 ,scale:0}}
+                    initial={{ y: height / 1.8, x: -width / 2, opacity: 0.25, scale: 0 }}
+                    animate={{ x: 0, y: 0, opacity: 1, scale: 1 }}
+                    exit={{ y: height / 1.8, x: -width / 2, opacity: 0, scale: 0 }}
                     transition={{ duration: 0.2 }}>
                     <Browser
                       handleCloseClick={handleCloseClick}
@@ -66,6 +66,38 @@ export default function Home() {
                 )}
               </AnimatePresence>
             )}
+
+            <div className='grid grid-rows-5 grid-flow-col gap-y-32 p-10 items-centers'>
+                <img src="/discord.png" className="object-scale-down h-12 w-12" alt="test"></img>
+                <img src="/github_edit.png" className="object-scale-down h-12 w-12"></img>
+                <img src="/Steam_icon_logo.svg.png" className="object-scale-down h-12 w-12"></img>
+                <img src="/twitteer.png" className="object-scale-down h-12 w-12"></img>
+
+                <img src="/discord.png" className="object-scale-down h-12 w-12" alt="test"></img>
+                <img src="/github_edit.png" className="object-scale-down h-12 w-12"></img>
+                <img src="/Steam_icon_logo.svg.png" className="object-scale-down h-12 w-12"></img>
+                <img src="/twitteer.png" className="object-scale-down h-12 w-12"></img>
+
+                <img src="/discord.png" className="object-scale-down h-12 w-12" alt="test"></img>
+                <img src="/github_edit.png" className="object-scale-down h-12 w-12"></img>
+                <img src="/Steam_icon_logo.svg.png" className="object-scale-down h-12 w-12"></img>
+                <img src="/twitteer.png" className="object-scale-down h-12 w-12"></img>
+                
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
             <div className="md:hidden block">
               <TopBar />
             </div>
