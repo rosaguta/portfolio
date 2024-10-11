e499b4fa7bc75ad2eb750c26800bab9b3b33fd6a
Rose <rvleeuwen@DigitalIndividuals.com>
2024-10-11 21:36:55 +0200

    tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,md,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,md,mdx}",
     "./src/markdown/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./markdown/**/*.{js,ts,jsx,tsx,md,mdx}"
   ],
   theme: {
     extend: {
