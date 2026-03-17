940bbb29b9c3d4e1dcd31848d293ba4cc7a697bf
Rose van Leeuwen <guta.rob@hotmail.nl>
2026-03-17 13:44:19 +0100

    animated password

--- src/components/Screens/LoginScreen.js
+++ src/components/Screens/LoginScreen.js
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion'
 import useIsMobile from '@/functions/useIsMobile'
 import useWindowDimensions from '@/functions/useWindowDimensions.js'
@@ -10,25 +10,43 @@ import { Input } from '../ui/input';
 // used for the intro sequence
 const currentDateTime = new Date()
 const displayTime = new String(currentDateTime.getHours().toString().padStart(2, '0') + ":" + currentDateTime.getMinutes().toString().padStart(2, '0'))
+const passwordCharArray = "BlahajOnSale".split(""); // change to an easteregg :3
+
 export default function LoginScreenAnim() {
+  const [displayPassword, setDisplayPassword] = useState('');
+  const [displayPasswordIndex, setDisplayPasswordIndex] = useState(0);
+
+  useEffect(() => {
+    if (displayPasswordIndex >= passwordCharArray.length) return;
+
+    const timeout = setTimeout(() => {
+      setDisplayPassword(prev => prev + passwordCharArray[displayPasswordIndex]);
+      setDisplayPasswordIndex(prev => prev + 1);
+    }, Math.random() * 200 + 50);
+
+    return () => clearTimeout(timeout);
+  }, [displayPasswordIndex]);
 
   return (
     <div className="relative h-screen">
-      <div className="absolute inset-0 bg-[url('/Rosey-stars.png')] bg-cover bg-center blur-md"></div>
-
+      <div className="absolute inset-0 bg-[url('/Rosey-stars.png')] bg-cover bg-center blur-md" />
       <div className="relative flex space-y-4 flex-col items-center justify-center h-full">
-        <div className='font-mono text-9xl text-rose-100 opacity-80 h-full pt-24'>
-          {displayTime}
+        <div className='font-mono text-9xl text-rose-100 opacity-80 h-full pt-36'> {displayTime} </div>
+        <div className='pb-5'>
+          <Image src={Rose_Headpat} width={150} height={150}/>
         </div>
-        <div className='space-y-6 h-full'>
-          {/* <Image src={Rose_Headpat} width={250} height={250}/> */}
+        <div className='space-y-3 h-full flex flex-col items-center text-xl'>
           <p>Rose van Leeuwen</p>
-          <Input defaultValue="some password" type="password" className='w-56 bg-neutral-800 rounded-md text-white' />
+          <Input
+            value={displayPassword}
+            type="password"
+            className='w-56 bg-neutral-800 rounded-full text-white bg-transparent/20 p-2'
+            readOnly
+          />
         </div>
       </div>
-
     </div>
-  )
+  );
 }
 // used for the optional log in screen when the user wanted to "log out"
 export function LoginScreen() {
